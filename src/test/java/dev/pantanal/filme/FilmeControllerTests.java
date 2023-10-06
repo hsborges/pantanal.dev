9ecb7dab4f351f6376a9b62ae7855a17683ce583
Hudson S. Borges <hudsonsilbor@gmail.com>
2023-10-06 17:27:11 -0400

    feat: adiciona ci workflow

--- src/test/java/dev/pantanal/filme/FilmeControllerTests.java
+++ src/test/java/dev/pantanal/filme/FilmeControllerTests.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class FilmeControllerTests {
 
     @MockBean
--- src/test/java/dev/pantanal/filme/FilmeServiceTests.java
+++ src/test/java/dev/pantanal/filme/FilmeServiceTests.java
@@ -11,8 +11,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class FilmeServiceTests {
 
     @Mock
